51696c3b530e2e601b9af5eb399d6c1eb64545db
Trevor F. Freeman <trevor.freeman@utexas.edu>
2022-11-28 16:26:12 -0500

    Add test for k-mer counting

---
+++ tests/test_ch01.py
@@ -0,0 +1,13 @@
+from bioinformatics_textbook.code_challenges.ch01 import (
+    count_pattern
+)
+
+
+def test_count_pattern():
+    input_text = "GCGCG"
+    input_pattern = "GCG"
+
+    expected_output = 2
+    actual_output = count_pattern(text=input_text, pattern=input_pattern)
+
+    assert expected_output == actual_output
