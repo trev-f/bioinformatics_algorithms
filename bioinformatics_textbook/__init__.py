ec6fe47d8bbb585e9ec16a2ff2cd0223a7cf6cd7
Trevor F. Freeman <trevor.freeman@utexas.edu>
2022-11-28 16:25:28 -0500

    Refactor code challenges module structure

--- bioinformatics_textbook/__init__.py
+++ bioinformatics_textbook/__init__.py
@@ -2,4 +2,4 @@ import bioinformatics_textbook.data
 import bioinformatics_textbook.features
 import bioinformatics_textbook.models
 import bioinformatics_textbook.visualization
-import bioinformatics_textbook.ch01
+import bioinformatics_textbook.code_challenges
