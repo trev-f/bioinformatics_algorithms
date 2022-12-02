1aa6c7b129879af2c1d3beb6fccc1ad3a5328d37
Trevor F. Freeman <trevor.freeman@utexas.edu>
2022-12-02 13:21:53 -0500

    Doc read last line function

--- bioinformatics_textbook/code_challenges/io.py
+++ bioinformatics_textbook/code_challenges/io.py
@@ -11,6 +11,13 @@ def read_text_pattern(input_file: click.File) -> tuple:
 
 
 def read_last_line(input_file: click.File) -> str:
+    """Read the last line of a file
+
+    :param input_file: The input file. Must be opened for reading in binary mode.
+    :type input_file: click.File
+    :return: The last line.
+    :rtype: str
+    """
     input_file.seek(-2, os.SEEK_END)
     while input_file.read(1) != b"\n":
         input_file.seek(-2, os.SEEK_CUR)
