2ef8686331ff0e9588ab5bff63949fce134c7d16
Trevor F. Freeman <trevor.freeman@utexas.edu>
2022-12-02 13:07:00 -0500

    Add read last line of a file

--- bioinformatics_textbook/code_challenges/io.py
+++ bioinformatics_textbook/code_challenges/io.py
@@ -1,5 +1,20 @@
 import click
+import os
 
 
-def read_text_pattern(file: click.File) -> tuple:
-    return (file, 0)
+def read_text_pattern(input_file: click.File) -> tuple:
+    other_lines = ""
+    # get the last line
+    last_line = read_last_line(input_file)
+
+    return (other_lines, last_line)
+
+
+def read_last_line(input_file: click.File) -> str:
+    input_file.seek(-2, os.SEEK_END)
+    while input_file.read(1) != b"\n":
+        input_file.seek(-2, os.SEEK_CUR)
+    
+    last_line = input_file.readline().decode().rstrip()
+
+    return last_line
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,6 +1,7 @@
+import click
 import pytest
 from bioinformatics_textbook.code_challenges.io import (
-    read_text_pattern
+    read_last_line, read_text_pattern
 )
 
 
@@ -13,7 +14,20 @@ def test_read_text_pattern(ba1a_sample_dataset_path):
     expected_text = "GCGCG"
     expected_pattern = "GCG"
 
-    actual_text, actual_pattern = read_text_pattern(ba1a_sample_dataset_path)
+    with click.open_file(ba1a_sample_dataset_path) as file:
+        actual_text, actual_pattern = read_text_pattern(file)
 
     assert expected_text == actual_text
     assert expected_pattern == actual_pattern
+
+
+def test_read_last_line(fs):
+    # setup fake file
+    fake_file = fs.create_file("multiline_test.txt", contents="first line\nlast line")
+
+    expected_last_line = "last line"
+
+    with click.open_file(fake_file.path, "rb") as file:
+        actual_last_line = read_last_line(file)
+
+    assert expected_last_line == actual_last_line
