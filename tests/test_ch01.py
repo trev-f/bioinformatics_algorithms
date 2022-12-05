8b74c230314efeb78b9307b86ebc8a0de3d56387
Trevor F. Freeman <trevor.freeman@utexas.edu>
2022-12-05 11:59:59 -0500

    Add test for finding most frequent words

--- tests/test_ch01.py
+++ tests/test_ch01.py
@@ -1,6 +1,25 @@
 from bioinformatics_textbook.code_challenges.ch01 import (
-    construct_kmer_freq_table, count_pattern
+    construct_kmer_freq_table, count_pattern, find_frequent_words
 )
+import pytest
+
+
+@pytest.fixture
+def sample_text_k():
+    text = "ACGTTTCACGTTTTACGG"
+    k = 3
+
+    yield text, k
+
+
+def test_find_frequent_words(sample_text_k):
+    input_text, input_k = sample_text_k
+    expected_frequent_words = "ACG TTT"
+
+    actual_frequent_words = find_frequent_words(text=input_text, k=input_k)
+
+    assert expected_frequent_words == actual_frequent_words
+
 
 def test_construct_kmer_freq_table():
     input_text = "ACGTTTCACGTTTTACGG"
