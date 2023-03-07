166376054c030ab2b38cb5fa7384df78486f8b65
Trevor Freeman <trvrfreeman@gmail.com>
2023-03-07 21:16:41 +0000

    Add solve BA2A motif enumeration

--- bioinformatics_textbook/ch02/__init__.py
+++ bioinformatics_textbook/ch02/__init__.py
@@ -3,11 +3,11 @@ from bioinformatics_textbook.ch02.motif import Motif
 
 
 class BA2A(RosalindSolution):
-    def _solve_problem(self) -> str:
-        k_d_motifs = Motif(
+    def _solve_problem(self) -> set:
+        k_d_motifs = Motif().find_k_d_motifs(
             kmer_length=self.dataset.kmer_length,
             num_allowed_mismatches=self.dataset.num_allowed_mismatches,
             dnas=self.dataset.dnas,
-        ).find_k_d_motifs()
+        )
 
         return self._format_rosalind_answer(k_d_motifs)
